5713e271fc61461fa7c751b921762b28a4af7346
marucomerci <marianacomerci@oceano.com.ar>
2019-05-16 19:28:36 -0300

    arreglé el flex de las pelis

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 const apiKey = "dc7a4681a677b75449e394ed80e327f8";
 
-fetch(`https://api.themoviedb.org/3/movie/550?api_key=dc7a4681a677b75449e394ed80e327f8=${apiKey}`)
+fetch(`https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`)
 .then(res => res.json())
       .then(data =>{ console.log(data); })
       
