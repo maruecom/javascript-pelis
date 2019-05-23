1aba5f5794d4e0b4303f91ee8817eb8c37db93a6
marucomerci <marianacomerci@oceano.com.ar>
2019-05-23 19:17:29 -0300

    boton top rated

--- javascript.js
+++ javascript.js
@@ -140,6 +140,52 @@ document.querySelector("#sec-popular").onclick = function (){
 }
 
 
+// aprieto boton lateral top rated
+
+
+
+document.querySelector("#sec-top").onclick = function (){
+ 
+  const ocultar = document.getElementsByClassName("batman")[0].classList.add("desaparece");
+  const ocultarDos = document.querySelector("#see-allUno").classList.add("desaparece");
+  const ocultarDosA = document.querySelector("#see-allTres").classList.add("desaparece");
+  const ocultarDosB = document.querySelector("#see-allCuatro").classList.add("desaparece");
+  const ocultarTres = document.getElementById("pelicula").classList.add("desaparece");
+  const ocultarCuatro = document.getElementById("pelicula3").classList.add("desaparece");
+  const ocultarCinco= document.getElementById("pelicula4").classList.add("desaparece");
+
+  
+  fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
+  .then(res => res.json())
+        .then(data =>{ console.log(data); 
+  
+          const movie = data.results;
+          const div = document.getElementById("pelicula2");
+  
+          for (let i = 0; i < movie.length; i++) {
+            if(i < 20){
+              div.innerHTML += `<div class="peli"><img src="https://image.tmdb.org/t/p/original${movie[i].poster_path}">
+              <h1 class="titu-peli">${movie[i].original_title}</h1></div>`
+             
+            }
+            
+            
+          }
+            
+          const boton = document.createElement ("button");
+          const papaBoton = document.querySelector("#pelicula2");
+          papaBoton.appendChild(boton);
+          boton.innerHTML = "LOAD MORE";
+          boton.classList.add("boton");
+          document.querySelector("#sec-top").onclick = function (){
+            
+          }
+        })
+
+}
+
+
+
 
   
 
